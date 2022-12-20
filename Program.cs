e655101e79861a477bfbf945690d441a65cd9c77
PishchNatalia <n.pisch@mail.ru>
2022-12-20 13:46:58 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ void PrintArray(double[,]array)
 */
 
 // Задача 2.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
+/*
 
 Console.WriteLine("Введите номер строки : ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -81,4 +81,51 @@ void PrintArray(int [,]array)
     }
 }
 
+*/
+
 // Задача 3.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] numbers = new int [m,n];
+FillArrayRandomNumbers(numbers);
+
+for ( int j = 0;j < numbers.GetLength(1);j++)
+{
+    double avarage = 0;
+    for (int i = 0;i < numbers.GetLength(0);i++)
+    {
+        avarage = (avarage + numbers[i,j]);
+    }
+        avarage = avarage / m;
+        Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for ( int i = 0; i < array.GetLength(0);i++)
+    {
+        for (int j = 0; j < array.GetLength(1);j++)
+        {
+            array[i,j] = new Random().Next( 0 ,10);
+        }
+    }
+}
+void PrintArray(int [,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" [ ");
+        for (int j = 0 ;j < array.GetLength(1);j++)
+        {
+            Console.Write(array[i,j] + "  ");
+        }
+        Console.Write("]");
+        Console.WriteLine(" ");
+    }
+}
