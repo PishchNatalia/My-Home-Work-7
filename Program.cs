3f643790af4e01e756d26da0265b9daee79d969e
PishchNatalia <n.pisch@mail.ru>
2022-12-20 13:05:23 +0300

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Домашняя работа 7
+
+// Задача 1.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] numbers = new double[m,n];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    { 
+        for(int j = 0;j < array.GetLength(1);j++)
+        {
+            array[i,j] = Convert.ToDouble(new Random().Next(-100,100))/10;
+        }
+    }
+}
+void PrintArray(double[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" [ ");
+        for (int j = 0 ;j < array.GetLength(1);j++)
+        {
+            Console.Write(array[i,j] + "  ");
+        }
+        Console.Write("]");
+        Console.WriteLine(" ");
+    }
+}
+
+
+// Задача 2.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Задача 3.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
